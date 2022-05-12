62431932b4597770200ed7da6deaff8b0c84ea83
Michael Douglas <michaeldougls93@gmail.com>
2022-05-12 10:13:02 +0100

    ok

--- modulo2/insta/insta4/insta4/src/App.js
+++ modulo2/insta/insta4/insta4/src/App.js
@@ -13,8 +13,7 @@ class App extends React.Component {
   render() {
     return (
       <MainContainer>
-        <Posts://picsum.pho
-          fost={'https://picsum.photos/200/150'}
+        <Post fost={'https://picsum.photos/200/150'}
         />
       </MainContainer>
     );
