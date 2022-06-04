d29cff1c36f2800e21b58eacf8a4802af4767d45
Michael Douglas <michaeldougls93@gmail.com>
2022-06-04 13:41:29 +0100

    inicio projeto, sistema bancario

--- modulo4/semana2/IWFS-bank/src/index.ts
+++ modulo4/semana2/IWFS-bank/src/index.ts
@@ -1,181 +1,52 @@
-import express, { Request, Response } from 'express'
-import cors from 'cors'
-import { users } from './data';
+import express, { Request, Response } from "express";
+import cors from "cors";
+import { accounts } from "./accounts";
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-// -----------------------------------
+app.post("/users/create", (req, res) => {
+  try {
+    const { name, CPF, dateOfBirthAsString } = req.body;
 
+    const [day, month, year] = dateOfBirthAsString.split("/");
 
+    const dateOfBirth: Date = new Date(`${year}-${month}-${day}`);
+    // validar entradas
+    const ageInMilliseconds:number = Date.now() - dateOfBirth.getTime()
+    const ageInYears:number = ageInMilliseconds/1000 /60 /60 /24/365
 
-
-// todos usuarios
-app.get("/users/allUser", (req, res) => {
-    res.send('retornando todos os usarios')
-})
-
-// trazer um usuario especifico
-app.get("/users/:id", (req, res) => {
-    const id = Number(req.params.id)
-    res.send(`o usuario ${id} foi encontrado`)
-})
-
-
-
-
-app.get('/playlists', (req, res) => {
-    // vai retornar todas as playlists
-
-    const playlists = users.map((user) => {
-        return user.playlists;
-    })
-
-    const resultado = playlists.flat(1);
-
-    // flat faz isso aqui > [1, [2, 3], 4] = [1, 2, 3, 4]
-
-    res.send(resultado);
-});
-
-app.get('/playlists/:id', (req, res) => {
-    const id = req.params.id
-
-    const findPlaylist = users.filter(
-        (user) => {
-            //    if(user.id === id){
-            //        return true
-            //    }
-
-            return user.id === id
-        })
-
-    res.status(200).send(findPlaylist)
-})
-
-app.post('/createPlaylist/:id', (req, res) => {
-    type Tracks = {
-        id: string,
-        name: string,
-        artist: string,
-        url: string
+    if(ageInYears < 18){
+        res.statusCode =406
+        throw new Error ("Idade tem que ser mais que 18 anos")
     }
-    type NewPlaylist = {
-        id: string,
-        name: string,
-        tracks: Tracks[]
-    }
-
-    const idPerson = req.params.id
-    const { id, name, tracks }: NewPlaylist = req.body
-
-    // sem tipagem
-    // const { id, name } = req.body
-    // const tracks: Tracks[] = []
-
-    const findPlaylist = users.find((user) => {
-        return user.id === idPerson
-    })
-
-    const newPlaylist = {
-        id,
-        name,
-        tracks
-    }
-
-    findPlaylist?.playlists.push(newPlaylist)
-
-    res.status(201).send(findPlaylist)
-})
-
-app.post('/createTrack/:id', (req, res) => {
-    const idPerson = req.params.id
-    const { idPlaylist } = req.query
-
-    const { id, name, artist, url } = req.body
-
-    // achar a pessoa dentre varias
-    const [findPerson] = users.filter((user) => {
-        return user.id === idPerson
-    })
-
-    // Achar a playlist da minha pessoa
-    const findPlaylist = findPerson.playlists.filter((play) => {
-        if(play.id === idPlaylist){
-            play.tracks.push({id,name,artist,url})
-            return true
-        }
-    })
-
-    res.send(findPlaylist)
-
-})
-
-app.get('/tracks', (req, res) => {
-    // vai retornar todas as tracks
-
-    const playlists = users.map((user) => {
-        return user.playlists
-    }).flat(1);
-
-    // peguei as playlists
-
-    const tracks = playlists.map((playlist) => {
-        return playlist.tracks;
-    }).flat(1);
-
-    // peguei as tracks das playlists e juntei em um array
-
-    res.send(tracks);
-});
-
-app.delete("/playlists/:userId", (req: Request, res: Response) => {
-    // deletar as playlists
-
-    const userId = req.params.userId;
-
-    const dadosAtualizados = users.map((user) => {
-        if (user.id === userId) {
-            return {
-                ...user,
-                playlists: []
-            }
-        } else {
-            return user;
-        }
+    //consultar  ou alterar base de dados
+    
+    accounts.push({
+      name,
+      CPF,
+      dateOfBirth,
+      balance: 0,
+      statement: [],
     });
-
-    res.send(dadosAtualizados);
+    res.status(201).send("Conta Criada com sucesso!");
+  } catch (error: any) {
+    res.status(400).send(error.message);
+  }
 });
-
-app.delete('/tracks/:playlistId', (req, res) => {
-    //deletar as tracks
-    const playlistId = req.params.playlistId;
-
-    const dadosAtualizados = users.map((user) => {
-        return user.playlists.map((playlist) => {
-            if (playlist.id === playlistId) {
-                return {
-                    ...playlist,
-                    tracks: []
-                }
-            } else {
-                return playlist
-            }
-        })
-    });
-
-
-    res.send(dadosAtualizados);
+app.get("/users/all", (req, res) => {
+  try {
+    if (!accounts.length) {
+      res.statusCode = 404;
+      throw new Error("Nenhuma conta encontrada");
+    }
+    res.status(200).send(accounts);
+  } catch (error: any) {
+    res.status(400).send(error.message);
+  }
 });
-
-
-
 app.listen(3003, function () {
-    console.log("Server is running in locahost:3003 !");
+  console.log("Server is running in locahost:3003 !");
 });
-
-
-
