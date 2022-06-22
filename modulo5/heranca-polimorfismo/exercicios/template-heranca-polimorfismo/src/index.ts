0f93e11b7d49c17a7a93d04d1a7f610a84c4c4d2
Michael Douglas <michaeldougls93@gmail.com>
2022-06-22 17:30:11 +0100

    exercicio de heranca terminados

--- modulo5/heranca-polimorfismo/exercicios/template-heranca-polimorfismo/src/index.ts
+++ modulo5/heranca-polimorfismo/exercicios/template-heranca-polimorfismo/src/index.ts
@@ -1,35 +1,37 @@
-import { Dog } from "./Dog";
-import { Owl } from "./Owl";
+// import { Dog } from "./Dog";
+// import { Owl } from "./Owl";
 
 
-import { Character } from "./Character";
-import Animal from "./Animal";
+// import { Character } from "./Character";
+// import Animal from "./Animal";
+import User from './User';
+import Customer from './Customer';
 
-const scooby = new Dog("Scooby Doo", 20, "Vira-lata")
+// const scooby = new Dog("Scooby Doo", 20, "Vira-lata")
 
-scooby.bark()
+// scooby.bark()
 
-scooby.eat(4)
+// scooby.eat(4)
 
-console.log(Dog.racas)
+// console.log(Dog.racas)
 
-console.log(Date.now())
+// console.log(Date.now())
 
-const edwiges = new Owl("Edwiges", 4)
+// const edwiges = new Owl("Edwiges", 4)
 
-edwiges.chirp()
-edwiges.fly(4)
-edwiges.eat(4)
+// edwiges.chirp()
+// edwiges.fly(4)
+// edwiges.eat(4)
 
-const pluto = new Dog("Pluto", 20, "Pastor Alemão")
+// const pluto = new Dog("Pluto", 20, "Pastor Alemão")
 
-const nemo = new Animal("Nemo", 5)
+// const nemo = new Animal("Nemo", 5)
 
-const zoo: Animal[] = [scooby, pluto, edwiges, nemo]
+// const zoo: Animal[] = [scooby, pluto, edwiges, nemo]
 
-console.log(zoo)
+// console.log(zoo)
 
-console.log(scooby instanceof Animal)
+// console.log(scooby instanceof Animal)
 // Character.MAX_HEALTH = 1000
 
 // const ryu = new Character("Ryu", 20, 30)
@@ -45,3 +47,22 @@ console.log(scooby instanceof Animal)
 // console.log(mario);
 
 // const toto = new Dog("toto", 20, Dog.racas[1])
+// /Exercício 1
+// const newUser = new User("3232","miked@gail.com","mike","32e3msddf")
+// console.log(newUser);
+// a) Seria possível imprimir a senha (password) atrelada a essa instância?
+// a propriedade so pode ser acessada na classse pq e private
+//b) Quantas vezes a mensagem "Chamando o construtor da classe User" foi impressa no terminal? 1 vez
+// /Exercício 2
+
+// const newCostumer = new Customer("3232","miked@gail.com","mike","32e3msddf","1300 reais")
+// console.log(newCostumer);
+// a) *Quantas vezes a mensagem `"Chamando o construtor da classe Customer"` foi impressa no terminal?* 1 vez
+
+// b) *Quantas vezes a mensagem `"Chamando o construtor da classe User"` foi impressa no terminal? Por quê?* 1 vez pq ela eh o pai de costumer
+//exercicio 3
+// a) Seria possível imprimir a senha (password) atrelada a essa instância? Por quê? nao pq ela e private
+
+//exercicio 4
+// const newUser = new User("3232","miked@gail.com","mike","32e3msddf")
+// console.log(newUser.getIntroduceYourself()
